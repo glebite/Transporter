6fe2c42374d836cf27496e9ab665ae2a1cd24f51
Lazlo <glebite@gmail.com>
2019-12-14 11:26:07 -0500

    Added tests - commented out old procedural code for refactor

--- src/transporter.py
+++ src/transporter.py
@@ -8,51 +8,51 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
-# Define these once; use them twice!
-strFrom = sys.argv[2]
-strTo = sys.argv[3]
-password = sys.argv[4]
+# # Define these once; use them twice!
+# strFrom = sys.argv[2]
+# strTo = sys.argv[3]
+# password = sys.argv[4]
 
-# Create the root message and fill in the from, to, and subject headers
-msgRoot = MIMEMultipart('related')
-msgRoot['Subject'] = sys.argv[1]
-msgRoot['From'] = strFrom
-sender = strFrom
-msgRoot['To'] = strTo
-receiver = strTo
-msgRoot.preamble = 'This is a multi-part message in MIME format.'
+# # Create the root message and fill in the from, to, and subject headers
+# msgRoot = MIMEMultipart('related')
+# msgRoot['Subject'] = sys.argv[1]
+# msgRoot['From'] = strFrom
+# sender = strFrom
+# msgRoot['To'] = strTo
+# receiver = strTo
+# msgRoot.preamble = 'This is a multi-part message in MIME format.'
 
-# Encapsulate the plain and HTML versions of the message body in an
-# 'alternative' part, so message agents can decide which they want to display.
-msgAlternative = MIMEMultipart('alternative')
-msgRoot.attach(msgAlternative)
+# # Encapsulate the plain and HTML versions of the message body in an
+# # 'alternative' part, so message agents can decide which they want to display.
+# msgAlternative = MIMEMultipart('alternative')
+# msgRoot.attach(msgAlternative)
 
-msgText = MIMEText('This is the alternative plain text message.')
-msgAlternative.attach(msgText)
+# msgText = MIMEText('This is the alternative plain text message.')
+# msgAlternative.attach(msgText)
 
-string_message = 'Security images.'
-counter = 0
-for image_name in sys.argv[5:]:
-    print('Working on image: ' + image_name)
-    # We reference the image in the IMG SRC attribute by the ID we give it below
-    string_message += '<img src="cid:image{}"><br>'.format(counter)
-    fp = open(image_name,'rb')
-    msgImage = MIMEImage(fp.read(), _subtype="jpg")
-    fp.close()
-    msgImage.add_header('Content-ID', '<image{}>'.format(counter))
-    msgRoot.attach(msgImage)
-    counter += 1
+# string_message = 'Security images.'
+# counter = 0
+# for image_name in sys.argv[5:]:
+#     print('Working on image: ' + image_name)
+#     # We reference the image in the IMG SRC attribute by the ID we give it below
+#     string_message += '<img src="cid:image{}"><br>'.format(counter)
+#     fp = open(image_name,'rb')
+#     msgImage = MIMEImage(fp.read(), _subtype="jpg")
+#     fp.close()
+#     msgImage.add_header('Content-ID', '<image{}>'.format(counter))
+#     msgRoot.attach(msgImage)
+#     counter += 1
 
-msgText = MIMEText(string_message, 'html')
-msgAlternative.attach(msgText)
+# msgText = MIMEText(string_message, 'html')
+# msgAlternative.attach(msgText)
 
-smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
-smtpserver.ehlo()
-smtpserver.starttls()
-smtpserver.ehlo
-smtpserver.login(sender, password)
-smtpserver.sendmail(sender, receiver, msgRoot.as_string())
-smtpserver.close()
+# smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+# smtpserver.ehlo()
+# smtpserver.starttls()
+# smtpserver.ehlo
+# smtpserver.login(sender, password)
+# smtpserver.sendmail(sender, receiver, msgRoot.as_string())
+# smtpserver.close()
 
 class Transporter:
     """ Transporter """
---
+++ test/test_emailer.py
@@ -0,0 +1,13 @@
+"""
+
+"""
+import sys
+import pytest
+sys.path.append('../src')
+from transporter import Transporter
+
+@pytest.mark.test_id(1)
+def test_Transporter_creation():
+    trans_obj = Transporter()
+    assert trans_obj is not None
+
