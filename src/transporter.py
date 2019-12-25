2dd57cd558561ce3e71ed15a231a542d4018d915
Lazlo <glebite@gmail.com>
2019-12-25 13:47:04 -0500

    got it working - just need to clean up some repetitive and redudant code, a bit of flexibility but otherwise should work fine!

--- src/transporter.py
+++ src/transporter.py
@@ -11,68 +11,57 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
-# # Create the root message and fill in the from, to, and subject headers
-# msgRoot = MIMEMultipart('related')
-# msgRoot['Subject'] = sys.argv[1]
-# msgRoot['From'] = strFrom
-# sender = strFrom
-# msgRoot['To'] = strTo
-# receiver = strTo
-# msgRoot.preamble = 'This is a multi-part message in MIME format.'
-
-# # Encapsulate the plain and HTML versions of the message body in an
-# # 'alternative' part, so message agents can decide which they want to display.
-# msgAlternative = MIMEMultipart('alternative')
-# msgRoot.attach(msgAlternative)
-
-# msgText = MIMEText('This is the alternative plain text message.')
-# msgAlternative.attach(msgText)
-
-# string_message = 'Security images.'
-
-# msgText = MIMEText(string_message, 'html')
-# msgAlternative.attach(msgText)
-
-# smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
-
 class Transporter:
     """ Transporter """
-    def __init__(self, from_address=None, to_address=None, user_password=None):
+    def __init__(self, from_address=None, to_address=None, user_password=None, subject=None):
         """ init function """
-        self.sender = from_address
-        self.receiver = to_address
+        self.msg_root = MIMEMultipart('related')
+        self.msg_root['From']  = from_address
+        self.msg_root['To'] = to_address
+        self.msg_root['Subject'] = subject
         self.password = user_password
-        self.string_message = ""
-        self.msg_text = MIMEMultipart('related')
+        self.receiver = to_address
+        self.sender = from_address
+        self.string_message = "Test"
+        self.msg_root.preamble = 'This is a multi-part message in MIME format.'
+        self.msg_alternative = MIMEMultipart('alternative')
+        self.msg_root.attach(self.msg_alternative)
+        self.msg_text = MIMEText('This is the alternative plain text message.')
+        self.msg_alternative.attach(self.msg_text)
 
-    def build_message_text(self, subject_text=None,
+    def build_message_text(self, subject_text="",
                            preamble_text="",
                            string_message=""):
-        """ build_message_text """
+        """pi build_message_text """
         self.string_message += string_message
 
     def add_images(self, images=None):
         """ add_images """
         counter = 0
+        string_message = "<br>Security images...<br>"
         for image_name in images:
-            self.string_message += '<img src="cid:image{}"><br>'.format(counter)
+            print(f'Working on {image_name}')
+            string_message += '<img src="cid:image{}"><br>{}<br>'.format(counter, image_name)
             file_pointer = open(image_name, 'rb')
-            msg_image = MIMEImage(file_pointer.read(), _subtype="jpg")
+            msg_image = MIMEImage(file_pointer.read(), _subtype="png")
             file_pointer.close()
             msg_image.add_header('Content-ID', '<image{}>'.format(counter))
-            self.msg_text.attach(msg_image)
+            self.msg_root.attach(msg_image)
             counter += 1
+        self.msg_text = MIMEText(string_message, 'html')
+        self.msg_alternative.attach(self.msg_text)
+        print(self.msg_root)
 
     def send_it(self):
         """ send it """
         smtp_server = SMTP('smtp.gmail.com', 587)
         smtp_server.ehlo()
         smtp_server.starttls()
-        smtp_server.ehlo()
+        smtp_server.ehlo
         smtp_server.login(self.sender, self.password)
         smtp_server.sendmail(self.sender,
-                             self.receiver,
-                             self.msg_text.as_string())
+                             self.msg_root['To'],
+                             self.msg_root.as_string())
         smtp_server.close()
 
 
@@ -82,7 +71,7 @@ if __name__ == "__main__":
     USER_PASSWORD = sys.argv[3]
     SUBJECT = sys.argv[4]
     IMAGES = sys.argv[5:]
-    T_OBJ = Transporter(FROM, TO, USER_PASSWORD)
-    T_OBJ.build_message_text(subject_text=SUBJECT)
+    T_OBJ = Transporter(FROM, TO, USER_PASSWORD, 'Hi there')
+    # T_OBJ.build_message_text(subject_text=SUBJECT)
     T_OBJ.add_images(IMAGES)
     T_OBJ.send_it()
