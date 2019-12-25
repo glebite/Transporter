493988e1feff8284c4c9f26e46ad1c4cfb6be649
Lazlo <glebite@gmail.com>
2019-12-25 18:33:23 -0500

    cleaned up and got rid of a few redundant variables

--- src/transporter.py
+++ src/transporter.py
@@ -20,25 +20,21 @@ class Transporter:
         self.msg_root['To'] = to_address
         self.msg_root['Subject'] = subject
         self.password = user_password
-        self.receiver = to_address
-        self.sender = from_address
-        self.string_message = "Test"
+        self.string_message = ""
         self.msg_root.preamble = 'This is a multi-part message in MIME format.'
         self.msg_alternative = MIMEMultipart('alternative')
         self.msg_root.attach(self.msg_alternative)
         self.msg_text = MIMEText('This is the alternative plain text message.')
         self.msg_alternative.attach(self.msg_text)
 
-    def build_message_text(self, subject_text="",
-                           preamble_text="",
-                           string_message=""):
+    def build_message_text(self, string_message=""):
         """pi build_message_text """
         self.string_message += string_message
 
     def add_images(self, images=None):
         """ add_images """
         counter = 0
-        string_message = "<br>Security images...<br>"
+        string_message = "<br>{}<br>".format(self.string_message)
         for image_name in images:
             print(f'Working on {image_name}')
             string_message += '<img src="cid:image{}"><br>{}<br>'.format(counter, image_name)
@@ -58,8 +54,8 @@ class Transporter:
         smtp_server.ehlo()
         smtp_server.starttls()
         smtp_server.ehlo
-        smtp_server.login(self.sender, self.password)
-        smtp_server.sendmail(self.sender,
+        smtp_server.login(self.msg_root['From'], self.password)
+        smtp_server.sendmail(self.msg_root['From'],
                              self.msg_root['To'],
                              self.msg_root.as_string())
         smtp_server.close()
@@ -71,7 +67,7 @@ if __name__ == "__main__":
     USER_PASSWORD = sys.argv[3]
     SUBJECT = sys.argv[4]
     IMAGES = sys.argv[5:]
-    T_OBJ = Transporter(FROM, TO, USER_PASSWORD, 'Hi there')
-    # T_OBJ.build_message_text(subject_text=SUBJECT)
+    T_OBJ = Transporter(FROM, TO, USER_PASSWORD, 'Text stuff...')
+    T_OBJ.build_message_text(string_message='Information...')
     T_OBJ.add_images(IMAGES)
     T_OBJ.send_it()
