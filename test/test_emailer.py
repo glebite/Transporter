bb09046bdb011f21a4c81042debd9851d19a46a5
Lazlo <glebite@gmail.com>
2019-12-25 19:44:19 -0500

    Added more tests just to be sure...

--- test/test_emailer.py
+++ test/test_emailer.py
@@ -1,3 +1,4 @@
+
 """
 
 """
@@ -11,3 +12,16 @@ def test_Transporter_creation():
     trans_obj = Transporter()
     assert trans_obj is not None
 
+@pytest.mark.test_id(2)
+def test_Transporter_field_validation():
+    trans_obj = Transporter(from_address='what@test.com', to_address='what2@test2.com', user_password='secret', subject='something')
+    assert trans_obj.msg_root['From'] == 'what@test.com'
+    assert trans_obj.msg_root['To'] == 'what2@test2.com'
+    assert trans_obj.msg_root['Subject'] == 'something'
+    assert trans_obj.password == 'secret'
+
+@pytest.mark.test_id(3)
+def test_Transporter_message_text():
+    trans_obj = Transporter()
+    trans_obj.build_message_text(string_message='this is a message')
+    assert trans_obj.string_message == 'this is a message'
